2cb6db4ccb60b3cacac207a30e91e7d25176db3f
myc0sis <myc0sis@github.com>
2026-01-29 11:33:57 -0500

    1

---
+++ quartz/components/Logo.tsx
@@ -0,0 +1,18 @@
+import { pathToRoot } from "../util/path"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { classNames } from "../util/lang"
+
+const Logo: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
+  const baseDir = pathToRoot(fileData.slug!)
+
+  return (
+    <div class={classNames(displayClass, "site-brand")}>
+      <a class="brand-link" href={baseDir} aria-label={cfg.pageTitle}>
+        <img class="brand-mark" src={`${baseDir}ss.svg`} alt="" />
+        <span class="brand-title">{cfg.pageTitle}</span>
+      </a>
+    </div>
+  )
+}
+
+export default (() => Logo) satisfies QuartzComponentConstructor
